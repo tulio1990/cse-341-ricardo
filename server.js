2f779ef119670acb4349f79841dacc9a8966ef1c
tulio1990 <trbanegas14@gmail.com>
2023-04-17 12:29:04 -0600

    second update - first assigment

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
+const routes = require('./routes');
 const port = process.env.PORT || 3000
 
-app.use('/', require('./routes'));
+app.use('/', routes);
 
 app.listen(port, () => {
-    console.log(`Server running ${port}`);
+    console.log(`Server running in ${port} port`);
 })
