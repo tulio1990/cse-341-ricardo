494bc6b7ce8c6abd12586efbe6197cfeafee1894
tulio1990 <trbanegas14@gmail.com>
2023-04-24 22:49:19 -0600

    upd lesson 3.1

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -64,7 +64,7 @@ const delContact = async (req, res) => {
   const response = await mongodb.getDb().db().collection('contacts').deleteOne({ _id: userId }, true);
   console.log(response);
   if (response.deletedCount > 0) {
-    res.status(204).send();
+    res.status(200).send();
   } else {
     res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
   }
