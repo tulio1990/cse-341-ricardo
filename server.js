f805316dcf8f4bcb98aadfd51b26f9944a49c6dd
tulio1990 <trbanegas14@gmail.com>
2023-04-28 15:56:00 -0600

    add punto y coma

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app
     res.setHeader('Content-Type', 'application/json');
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
     next();
-  })
+  });
   
 
   mongodb.initDb((err) => {
