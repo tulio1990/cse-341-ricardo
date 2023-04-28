2940d6e7e09f6ed7299119ad22608d4a7ac12f53
tulio1990 <trbanegas14@gmail.com>
2023-04-28 09:40:48 -0600

    UPD LESSON 4 (3)

--- server.js
+++ server.js
@@ -4,16 +4,24 @@ const bodyParser = require('body-parser');
 const mongodb = require('./db/conection');
 const port = process.env.PORT || 3000;
 
+// app
+//   .use(bodyParser.json())
+//   .use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader(
+//       'Access-Control-Allow-Headers',
+//       'Origin, X-Requested.With, Content-Type, Accept, Z-Key'
+//     );
+//     res.setHeader('Content-Type', 'application/json');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+//     next();
+//   })
+//   .use('/', require('./routes'));
+
 app
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested.With, Content-Type, Accept, Z-Key'
-    );
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
     next();
   })
   .use('/', require('./routes'));
